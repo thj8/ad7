43e853c944ee197745225881a31f9660e0458979
tanghaijun <tanghj@cyberpeace.cn>
2026-04-20 17:48:29 +0800

    feat: 定义 TopThreeProvider 接口

---
+++ plugins/topthree/provider.go
@@ -0,0 +1,22 @@
+package topthree
+
+import "context"
+
+// TopThreeProvider 定义 topthree 插件暴露给其他插件的接口
+type TopThreeProvider interface {
+	// GetBloodRank 获取用户在某道题目的三血排名
+	// 返回值: 1=一血, 2=二血, 3=三血, 0=未入榜
+	GetBloodRank(ctx context.Context, compID, chalID, userID string) (int, error)
+
+	// GetCompTopThree 获取比赛每道题目的三血信息
+	// 返回值: map[challengeID]BloodRankEntry
+	GetCompTopThree(ctx context.Context, compID string) (map[string]BloodRankEntry, error)
+}
+
+// BloodRankEntry 表示单道题目的三血排名信息
+type BloodRankEntry struct {
+	ChallengeID string
+	FirstBlood  string // 用户ID
+	SecondBlood string // 用户ID
+	ThirdBlood  string // 用户ID
+}
